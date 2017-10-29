824c01137f5fbcf25e48bde84941ce9d8b195f77
Tait Takatani <tait.takatani@gmail.com>
2017-10-29 12:06:11 -0700

    added overlay and play buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-$('article').on('click', function () {
+$('article').on('click', function (evt) {
+	if (evt.target.id === 'favorite') return;
   $('.modal').addClass('is-active');
 });
 
@@ -6,6 +7,16 @@ $('.close-modal').on('click', function () {
   $('.modal').removeClass('is-active');
 });
 
-$('.modal-background').on('click', function (evt) {
+$('.modal-background').on('click', function () {
   $('.modal').removeClass('is-active');
+});
+
+$('.favorite').on('click', function () {
+  if ($(this).hasClass('fa-star')) {
+  	$(this).removeClass('fa-star');
+  	$(this).addClass('fa-star-o');
+  } else {
+  	$(this).removeClass('fa-star-o');
+  	$(this).addClass('fa-star');
+  }
 });
