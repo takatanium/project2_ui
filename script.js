9e550937104f7db634dc3a25b750cb0d0875dd3e
Tait Takatani <tait.takatani@gmail.com>
2017-10-29 15:23:45 -0700

    note selection test

--- script.js
+++ script.js
@@ -20,3 +20,30 @@ $('.favorite').on('click', function () {
   	$(this).addClass('fa-star');
   }
 });
+
+//Circular Nav
+(function(){
+
+	var button = document.getElementById('cn-button'),
+    wrapper = document.getElementById('cn-wrapper');
+
+    //open and close menu when the button is clicked
+	var open = false;
+	button.addEventListener('click', handler, false);
+
+	function handler(){
+	  if(!open){
+	    // this.innerHTML = "Close";
+	    classie.add(wrapper, 'opened-nav');
+	  }
+	  else{
+	    // this.innerHTML = "Menu";
+		classie.remove(wrapper, 'opened-nav');
+	  }
+	  open = !open;
+	}
+	function closeWrapper(){
+		classie.remove(wrapper, 'opened-nav');
+	}
+
+})();
