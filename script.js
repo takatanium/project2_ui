2b8a5afde1e7fac76b6a1ff4eb8dc72d86a141dc
Tait Takatani <tait.takatani@gmail.com>
2017-11-01 17:33:41 -0700

    testing step select

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+$(document).ready( function () {
+  toggleSelectClasses($(window).width());
+});
+
+$(window).resize( function () { 
+	toggleSelectClasses($(window).width());
+});
+
+function toggleSelectClasses (winWidth) {
+  if (winWidth <= 768) {
+  	$('#step-selector').prop('multiple', false);
+  	$('#step-selector').removeAttr('size');
+  	$('#step-selector-div').removeClass('is-multiple');
+  } else {
+  	$('#step-selector').prop('multiple', 'true');
+  	$('#step-selector').attr('size', '8');
+  	$('#step-selector-div').addClass('is-multiple');  	
+  }
+}
+
 $('article').on('click', function (evt) {
 	if (evt.target.id === 'favorite') return;
   $('.modal').addClass('is-active');
@@ -20,6 +40,39 @@ $('.favorite').on('click', function () {
   	$(this).addClass('fa-star');
   }
 });
+
+$('.circle-modal').on('click', function () {
+	if ($(this).hasClass('unclicked')) {
+		if ($('#step-selector').val().length !== 0) {
+      $(this).html($('#step-selector').val());
+      $.each($("#step-selector option:selected"), function () {
+	      $(this).prop('selected', false);
+	    });
+		} else {
+		  $(this).removeClass('unclicked');
+		  $(this).addClass('clicked');
+	  }
+	} else {
+		$(this).removeClass('clicked');
+		$(this).addClass('unclicked');		
+	}
+});
+
+$('#step-selector').on('change', function () {
+	let that = this;
+	if ($('#step-grid').find('.clicked').length !== 0) {
+		$('.clicked').each(function () {
+	    $(this).html($(that).val());
+	    $(this).removeClass('clicked');
+	    $(this).addClass('unclicked');
+	  });
+	  $.each($("#step-selector option:selected"), function () {
+	    $(this).prop('selected', false);
+	  });
+	}
+});
+
+
 
 //Circular Nav
 (function(){
